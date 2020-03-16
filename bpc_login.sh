1545ddf9c7652efb876fccf5a096cbcdd487f84a
Peleg Sapir <peleg@ds.mpg.de>
2020-03-16 11:45:46 +0100

    Symlinked most

--- bpc_login.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/expect
-
-spawn openconnect vpn.mpibpc.mpg.de/webVPN
-expect "Username:"
-send "psapir@mpibpc.mpg.de\r"
-interact
--- update.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-sudo apt update
-sudo apt upgrade
-yes | sudo apt dist-upgrade
-
-i3 restart
